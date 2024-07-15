9dea0384946980789612704f8042d53284abe999
Mr-R-e-X <souvikhazra151@gmail.com>
2024-07-16 00:26:40 +0530

    assignment done

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,10 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+import ProductModel from "../models/product.model.js";
+export default class ProductController {
+  getProducts = (req, res) => {
+    // Write your code here
+    let products = new ProductModel().fetchProducts();
+    res.render("product", { products });
+  };
+}
